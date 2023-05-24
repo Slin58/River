dc59e22d5e61f18ac02f0e70f4a579d7685892ca
Nils Göbel <nils.goebel@student.fhws.de>
2023-05-24 16:35:26 +0200

    first configuration

---
+++ River.Data/Models/Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace River.Data.Models.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Adress { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ River.Data/Models/Repository/RiverContext.cs
@@ -0,0 +1,37 @@
+﻿using River.Data.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace River.Data.Models.Repository
+{
+    public class RiverContext
+    {
+        public RiverContext() 
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+                var connectionString = configuration
+                    .GetConnectionString("RiverContext");
+                optionsBuilder.UseSQLServer(connectionString);
+                base.onConfiguring(optionBuilder);
+            }
+        }
+    }
+}
