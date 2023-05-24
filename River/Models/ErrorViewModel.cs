df6e4caa29c96180bb379a036bcb2845cb4c1f43
Nils Göbel <nils.goebel@student.fhws.de>
2023-05-24 16:12:34 +0200

    Add project files.

---
+++ River/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace River.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
